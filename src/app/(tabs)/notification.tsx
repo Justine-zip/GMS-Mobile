764c5c71859a168e7f578d2f40e7485f06bec742
Justine <justinecarl612@gmail.com>
2026-03-12 10:53:01 +0800

    added custom card for notification filter

--- src/app/(tabs)/notification.tsx
+++ src/app/(tabs)/notification.tsx
@@ -1,16 +1,30 @@
-import { Text } from "react-native";
+import CustomButton from "@/src/components/custom_button";
+import CustomHeader from "@/src/components/custom_header";
+import { globalStyles } from "@/src/styles/globalStyles";
+import { ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
     <SafeAreaView
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={globalStyles.container}
     >
-      <Text>N O T I F I C A T I O N</Text>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        stickyHeaderIndices={[0]}
+      >
+        {/* Header */}
+        <CustomHeader header="Notification" icon="more-horizontal" isCircled={false}></CustomHeader>
+        {/* Notification Filter */}
+        <View style={[globalStyles.spacer, { flexDirection: 'row', gap: 8 }]}>
+          <CustomButton label="All" backgroundColor="#146EF5" textColor="#fff" />
+          <CustomButton label="Unread" backgroundColor="#fff" textColor="#000" />
+        </View>
+
+        <Text>New</Text>
+        {/* Notification Card */}
+
+      </ScrollView>
     </SafeAreaView>
   );
 }
