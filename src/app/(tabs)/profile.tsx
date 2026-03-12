3f1d76975cf0c9888cd40bfcc20355d1fac61564
Justine <justinecarl612@gmail.com>
2026-03-12 12:35:55 +0800

    refactor custom header adding iconSet

--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,15 +1,15 @@
+import CustomHeader from "@/src/components/custom_header";
+import { globalStyles } from "@/src/styles/globalStyles";
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
     <SafeAreaView
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={[globalStyles.container]}
     >
+      {/* Header */}
+      <CustomHeader header="Profile" icon="settings" iconSet="Feather" isCircled={false}></CustomHeader>
       <Text>P R O F I L E</Text>
     </SafeAreaView>
   );
